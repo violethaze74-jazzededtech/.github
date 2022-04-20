41868f6887ab83bb23597c7681908ace9339f493
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2022-04-20 19:43:29 -0400

    Initialize config-spike

---
+++ cmd/gh/main.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	surveyCore "github.com/AlecAivazis/survey/v2/core"
+	"github.com/cli/cli/api"
+	"github.com/cli/cli/internal/build"
+	"github.com/cli/cli/internal/config"
+	"github.com/cli/cli/internal/ghinstance"
+	"github.com/cli/cli/internal/run"
+	"github.com/cli/cli/pkg/cmd/alias/expand"
+	"github.com/cli/cli/pkg/cmd/factory"
+	"github.com/cli/cli/pkg/cmd/root"
+	"github.com/cli/cli/pkg/cmdutil"
+	"github.com/cli/cli/update"
+	"github.com/cli/cli/utils"
+	"github.com/cli/safeexec"
+	"github.com/mattn/go-colorable"
+	"github.com/mgutz/ansi"
+	"github.com/spf13/cobra"
+)
+
+var updaterEnabled = ""
+
+func main() {
+	buildDate := build.Date
+	buildVersion := build.Version
+
+	updateMessageChan := make(chan *update.ReleaseInfo)
+	go func() {
+		rel, _ := checkForUpdate(buildVersion)
+		updateMessageChan <- rel
+	}()
+
+	hasDebug := os.Getenv("DEBUG") != ""
+
+	if hostFromEnv := os.Getenv("GH_HOST"); hostFromEnv != "" {
+		ghinstance.OverrideDefault(hostFromEnv)
+	}
+
+	cmdFactory := factory.New(buildVersion)
+	stderr := cmdFactory.IOStreams.ErrOut
+	if !cmdFactory.IOStreams.ColorEnabled() {
+		surveyCore.DisableColor = true
+	} else {
+		// override survey's poor choice of color
+		surveyCore.TemplateFuncsWithColor["color"] = func(style string) string {
+			switch style {
+			case "white":
+				if cmdFactory.IOStreams.ColorSupport256() {
+					return fmt.Sprintf("\x1b[%d;5;%dm", 38, 242)
+				}
+				return ansi.ColorCode("default")
+			default:
+				return ansi.ColorCode(style)
+			}
+		}
+	}
+
+	// Enable running gh from Windows File Explorer's address bar. Without this, the user is told to stop and run from a
+	// terminal. With this, a user can clone a repo (or take other actions) directly from explorer.
+	if len(os.Args) > 1 && os.Args[1] != "" {
+		cobra.MousetrapHelpText = ""
+	}
+
+	rootCmd := root.NewCmdRoot(cmdFactory, buildVersion, buildDate)
+
+	cfg, err := cmdFactory.Config()
+	if err != nil {
+		fmt.Fprintf(stderr, "failed to read configuration:  %s\n", err)
+		os.Exit(2)
+	}
+
+	if prompt, _ := cfg.Get("", "prompt"); prompt == "disabled" {
+		cmdFactory.IOStreams.SetNeverPrompt(true)
+	}
+
+	if pager, _ := cfg.Get("", "pager"); pager != "" {
+		cmdFactory.IOStreams.SetPager(pager)
+	}
+
+	expandedArgs := []string{}
+	if len(os.Args) > 0 {
+		expandedArgs = os.Args[1:]
+	}
+
+	cmd, _, err := rootCmd.Traverse(expandedArgs)
+	if err != nil || cmd == rootCmd {
+		originalArgs := expandedArgs
+		isShell := false
+
+		expandedArgs, isShell, err = expand.ExpandAlias(cfg, os.Args, nil)
+		if err != nil {
+			fmt.Fprintf(stderr, "failed to process aliases:  %s\n", err)
+			os.Exit(2)
+		}
+
+		if hasDebug {
+			fmt.Fprintf(stderr, "%v -> %v\n", originalArgs, expandedArgs)
+		}
+
+		if isShell {
+			exe, err := safeexec.LookPath(expandedArgs[0])
+			if err != nil {
+				fmt.Fprintf(stderr, "failed to run external command: %s", err)
+				os.Exit(3)
+			}
+
+			externalCmd := exec.Command(exe, expandedArgs[1:]...)
+			externalCmd.Stderr = os.Stderr
+			externalCmd.Stdout = os.Stdout
+			externalCmd.Stdin = os.Stdin
+			preparedCmd := run.PrepareCmd(externalCmd)
+
+			err = preparedCmd.Run()
+			if err != nil {
+				if ee, ok := err.(*exec.ExitError); ok {
+					os.Exit(ee.ExitCode())
+				}
+
+				fmt.Fprintf(stderr, "failed to run external command: %s", err)
+				os.Exit(3)
+			}
+
+			os.Exit(0)
+		}
+	}
+
+	cs := cmdFactory.IOStreams.ColorScheme()
+
+	authCheckEnabled := os.Getenv("GITHUB_TOKEN") == "" &&
+		os.Getenv("GITHUB_ENTERPRISE_TOKEN") == "" &&
+		cmd != nil && cmdutil.IsAuthCheckEnabled(cmd)
+	if authCheckEnabled {
+		if !cmdutil.CheckAuth(cfg) {
+			fmt.Fprintln(stderr, cs.Bold("Welcome to GitHub CLI!"))
+			fmt.Fprintln(stderr)
+			fmt.Fprintln(stderr, "To authenticate, please run `gh auth login`.")
+			fmt.Fprintln(stderr, "You can also set the GITHUB_TOKEN environment variable, if preferred.")
+			os.Exit(4)
+		}
+	}
+
+	rootCmd.SetArgs(expandedArgs)
+
+	if cmd, err := rootCmd.ExecuteC(); err != nil {
+		printError(stderr, err, cmd, hasDebug)
+
+		var httpErr api.HTTPError
+		if errors.As(err, &httpErr) && httpErr.StatusCode == 401 {
+			fmt.Println("hint: try authenticating with `gh auth login`")
+		}
+
+		os.Exit(1)
+	}
+	if root.HasFailed() {
+		os.Exit(1)
+	}
+
+	newRelease := <-updateMessageChan
+	if newRelease != nil {
+		msg := fmt.Sprintf("%s %s → %s\n%s",
+			ansi.Color("A new release of gh is available:", "yellow"),
+			ansi.Color(buildVersion, "cyan"),
+			ansi.Color(newRelease.Version, "cyan"),
+			ansi.Color(newRelease.URL, "yellow"))
+
+		fmt.Fprintf(stderr, "\n\n%s\n\n", msg)
+	}
+}
+
+func printError(out io.Writer, err error, cmd *cobra.Command, debug bool) {
+	if err == cmdutil.SilentError {
+		return
+	}
+
+	var dnsError *net.DNSError
+	if errors.As(err, &dnsError) {
+		fmt.Fprintf(out, "error connecting to %s\n", dnsError.Name)
+		if debug {
+			fmt.Fprintln(out, dnsError)
+		}
+		fmt.Fprintln(out, "check your internet connection or githubstatus.com")
+		return
+	}
+
+	fmt.Fprintln(out, err)
+
+	var flagError *cmdutil.FlagError
+	if errors.As(err, &flagError) || strings.HasPrefix(err.Error(), "unknown command ") {
+		if !strings.HasSuffix(err.Error(), "\n") {
+			fmt.Fprintln(out)
+		}
+		fmt.Fprintln(out, cmd.UsageString())
+	}
+}
+
+func shouldCheckForUpdate() bool {
+	if os.Getenv("GH_NO_UPDATE_NOTIFIER") != "" {
+		return false
+	}
+	if os.Getenv("CODESPACES") != "" {
+		return false
+	}
+	return updaterEnabled != "" && !isCI() && !isCompletionCommand() && utils.IsTerminal(os.Stderr)
+}
+
+// based on https://github.com/watson/ci-info/blob/HEAD/index.js
+func isCI() bool {
+	return os.Getenv("CI") != "" || // GitHub Actions, Travis CI, CircleCI, Cirrus CI, GitLab CI, AppVeyor, CodeShip, dsari
+		os.Getenv("BUILD_NUMBER") != "" || // Jenkins, TeamCity
+		os.Getenv("RUN_ID") != "" // TaskCluster, dsari
+}
+
+func isCompletionCommand() bool {
+	return len(os.Args) > 1 && os.Args[1] == "completion"
+}
+
+func checkForUpdate(currentVersion string) (*update.ReleaseInfo, error) {
+	if !shouldCheckForUpdate() {
+		return nil, nil
+	}
+
+	client, err := basicClient(currentVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	repo := updaterEnabled
+	stateFilePath := path.Join(config.ConfigDir(), "state.yml")
+	return update.CheckForUpdate(client, stateFilePath, repo, currentVersion)
+}
+
+// BasicClient returns an API client for github.com only that borrows from but
+// does not depend on user configuration
+func basicClient(currentVersion string) (*api.Client, error) {
+	var opts []api.ClientOption
+	if verbose := os.Getenv("DEBUG"); verbose != "" {
+		opts = append(opts, apiVerboseLog())
+	}
+	opts = append(opts, api.AddHeader("User-Agent", fmt.Sprintf("GitHub CLI %s", currentVersion)))
+
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		if c, err := config.ParseDefaultConfig(); err == nil {
+			token, _ = c.Get(ghinstance.Default(), "oauth_token")
+		}
+	}
+	if token != "" {
+		opts = append(opts, api.AddHeader("Authorization", fmt.Sprintf("token %s", token)))
+	}
+	return api.NewClient(opts...), nil
+}
+
+func apiVerboseLog() api.ClientOption {
+	logTraffic := strings.Contains(os.Getenv("DEBUG"), "api")
+	colorize := utils.IsTerminal(os.Stderr)
+	return api.VerboseLog(colorable.NewColorable(os.Stderr), logTraffic, colorize)
+}
---
+++ pkg/cmd/pr/checks/checks_test.go
@@ -0,0 +1,202 @@
+package checks
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+
+	"github.com/cli/cli/internal/ghrepo"
+	"github.com/cli/cli/pkg/cmdutil"
+	"github.com/cli/cli/pkg/httpmock"
+	"github.com/cli/cli/pkg/iostreams"
+	"github.com/google/shlex"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCmdChecks(t *testing.T) {
+	tests := []struct {
+		name  string
+		cli   string
+		wants ChecksOptions
+	}{
+		{
+			name:  "no arguments",
+			cli:   "",
+			wants: ChecksOptions{},
+		},
+		{
+			name: "pr argument",
+			cli:  "1234",
+			wants: ChecksOptions{
+				SelectorArg: "1234",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			io, _, _, _ := iostreams.Test()
+			f := &cmdutil.Factory{
+				IOStreams: io,
+			}
+
+			argv, err := shlex.Split(tt.cli)
+			assert.NoError(t, err)
+
+			var gotOpts *ChecksOptions
+			cmd := NewCmdChecks(f, func(opts *ChecksOptions) error {
+				gotOpts = opts
+				return nil
+			})
+			cmd.SetArgs(argv)
+			cmd.SetIn(&bytes.Buffer{})
+			cmd.SetOut(&bytes.Buffer{})
+			cmd.SetErr(&bytes.Buffer{})
+
+			_, err = cmd.ExecuteC()
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.wants.SelectorArg, gotOpts.SelectorArg)
+		})
+	}
+}
+
+func Test_checksRun(t *testing.T) {
+	tests := []struct {
+		name    string
+		fixture string
+		stubs   func(*httpmock.Registry)
+		nontty  bool
+		wantOut string
+		wantErr string
+	}{
+		{
+			name: "no commits",
+			stubs: func(reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`query PullRequestByNumber\b`),
+					httpmock.StringResponse(`
+						{ "data": { "repository": {
+							"pullRequest": { "number": 123 }
+						} } }
+					`))
+			},
+			wantOut: "",
+			wantErr: "no commit found on the pull request",
+		},
+		{
+			name: "no checks",
+			stubs: func(reg *httpmock.Registry) {
+				reg.StubResponse(200, bytes.NewBufferString(`
+				  { "data": { "repository": {
+				  	"pullRequest": { "number": 123, "commits": { "nodes": [{"commit": {"oid": "abc"}}]}, "baseRefName": "master" }
+				  } } }
+				`))
+			},
+			wantOut: "",
+			wantErr: "no checks reported on the 'master' branch",
+		},
+		{
+			name:    "some failing",
+			fixture: "./fixtures/someFailing.json",
+			wantOut: "Some checks were not successful\n1 failing, 1 successful, and 1 pending checks\n\nX  sad tests   1m26s  sweet link\n✓  cool tests  1m26s  sweet link\n-  slow tests  1m26s  sweet link\n",
+			wantErr: "SilentError",
+		},
+		{
+			name:    "some pending",
+			fixture: "./fixtures/somePending.json",
+			wantOut: "Some checks are still pending\n0 failing, 2 successful, and 1 pending checks\n\n✓  cool tests  1m26s  sweet link\n✓  rad tests   1m26s  sweet link\n-  slow tests  1m26s  sweet link\n",
+			wantErr: "SilentError",
+		},
+		{
+			name:    "all passing",
+			fixture: "./fixtures/allPassing.json",
+			wantOut: "All checks were successful\n0 failing, 3 successful, and 0 pending checks\n\n✓  awesome tests  1m26s  sweet link\n✓  cool tests     1m26s  sweet link\n✓  rad tests      1m26s  sweet link\n",
+			wantErr: "",
+		},
+		{
+			name:    "with statuses",
+			fixture: "./fixtures/withStatuses.json",
+			wantOut: "Some checks were not successful\n1 failing, 2 successful, and 0 pending checks\n\nX  a status           sweet link\n✓  cool tests  1m26s  sweet link\n✓  rad tests   1m26s  sweet link\n",
+			wantErr: "SilentError",
+		},
+		{
+			name:   "no checks",
+			nontty: true,
+			stubs: func(reg *httpmock.Registry) {
+				reg.StubResponse(200, bytes.NewBufferString(`
+				  { "data": { "repository": {
+						"pullRequest": { "number": 123, "commits": { "nodes": [{"commit": {"oid": "abc"}}]}, "baseRefName": "master" }
+				  } } }
+				`))
+			},
+			wantOut: "",
+			wantErr: "no checks reported on the 'master' branch",
+		},
+		{
+			name:    "some failing",
+			nontty:  true,
+			fixture: "./fixtures/someFailing.json",
+			wantOut: "sad tests\tfail\t1m26s\tsweet link\ncool tests\tpass\t1m26s\tsweet link\nslow tests\tpending\t1m26s\tsweet link\n",
+			wantErr: "SilentError",
+		},
+		{
+			name:    "some pending",
+			nontty:  true,
+			fixture: "./fixtures/somePending.json",
+			wantOut: "cool tests\tpass\t1m26s\tsweet link\nrad tests\tpass\t1m26s\tsweet link\nslow tests\tpending\t1m26s\tsweet link\n",
+			wantErr: "SilentError",
+		},
+		{
+			name:    "all passing",
+			nontty:  true,
+			fixture: "./fixtures/allPassing.json",
+			wantOut: "awesome tests\tpass\t1m26s\tsweet link\ncool tests\tpass\t1m26s\tsweet link\nrad tests\tpass\t1m26s\tsweet link\n",
+			wantErr: "",
+		},
+		{
+			name:    "with statuses",
+			nontty:  true,
+			fixture: "./fixtures/withStatuses.json",
+			wantOut: "a status\tfail\t0\tsweet link\ncool tests\tpass\t1m26s\tsweet link\nrad tests\tpass\t1m26s\tsweet link\n",
+			wantErr: "SilentError",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			io, _, stdout, _ := iostreams.Test()
+			io.SetStdoutTTY(!tt.nontty)
+
+			opts := &ChecksOptions{
+				IO: io,
+				BaseRepo: func() (ghrepo.Interface, error) {
+					return ghrepo.New("OWNER", "REPO"), nil
+				},
+				SelectorArg: "123",
+			}
+
+			reg := &httpmock.Registry{}
+			defer reg.Verify(t)
+
+			if tt.stubs != nil {
+				tt.stubs(reg)
+			} else if tt.fixture != "" {
+				reg.Register(httpmock.GraphQL(`query PullRequestByNumber\b`), httpmock.FileResponse(tt.fixture))
+			}
+
+			opts.HttpClient = func() (*http.Client, error) {
+				return &http.Client{Transport: reg}, nil
+			}
+
+			err := checksRun(opts)
+			if err != nil {
+				assert.Equal(t, tt.wantErr, err.Error())
+			} else if tt.wantErr != "" {
+				t.Errorf("expected %q, got nil error", tt.wantErr)
+			}
+
+			assert.Equal(t, tt.wantOut, stdout.String())
+		})
+	}
+}
