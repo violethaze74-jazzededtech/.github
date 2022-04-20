b1ff0fca4b52478da0bdfc2ac1fd0d8a38eff0c3
Lisa Dudley <65783327+violethaze74@users.noreply.github.com>
2022-04-20 19:44:14 -0400

    Initialize pr-lookup-refactor

---
+++ pkg/cmd/pr/shared/finder_test.go
@@ -0,0 +1,132 @@
+package shared
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/cli/cli/context"
+	"github.com/cli/cli/internal/ghrepo"
+	"github.com/cli/cli/pkg/httpmock"
+)
+
+func TestFind(t *testing.T) {
+	type args struct {
+		baseRepoFn func() (ghrepo.Interface, error)
+		branchFn   func() (string, error)
+		remotesFn  func() (context.Remotes, error)
+		selector   string
+		fields     []string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		httpStub func(*httpmock.Registry)
+		wantPR   int
+		wantRepo string
+		wantErr  bool
+	}{
+		{
+			name: "number argument",
+			args: args{
+				selector: "13",
+				fields:   []string{"id", "number"},
+				baseRepoFn: func() (ghrepo.Interface, error) {
+					return ghrepo.FromFullName("OWNER/REPO")
+				},
+			},
+			httpStub: func(r *httpmock.Registry) {
+				r.Register(
+					httpmock.GraphQL(`query PullRequestByNumber\b`),
+					httpmock.StringResponse(`{"data":{"repository":{
+						"pullRequest":{"number":13}
+					}}}`))
+			},
+			wantPR:   13,
+			wantRepo: "https://github.com/OWNER/REPO",
+		},
+		{
+			name: "number only",
+			args: args{
+				selector: "13",
+				fields:   []string{"number"},
+				baseRepoFn: func() (ghrepo.Interface, error) {
+					return ghrepo.FromFullName("OWNER/REPO")
+				},
+			},
+			httpStub: nil,
+			wantPR:   13,
+			wantRepo: "https://github.com/OWNER/REPO",
+		},
+		{
+			name: "number with hash argument",
+			args: args{
+				selector: "#13",
+				fields:   []string{"id", "number"},
+				baseRepoFn: func() (ghrepo.Interface, error) {
+					return ghrepo.FromFullName("OWNER/REPO")
+				},
+			},
+			httpStub: func(r *httpmock.Registry) {
+				r.Register(
+					httpmock.GraphQL(`query PullRequestByNumber\b`),
+					httpmock.StringResponse(`{"data":{"repository":{
+						"pullRequest":{"number":13}
+					}}}`))
+			},
+			wantPR:   13,
+			wantRepo: "https://github.com/OWNER/REPO",
+		},
+		{
+			name: "URL argument",
+			args: args{
+				selector:   "https://example.org/OWNER/REPO/pull/13/files",
+				fields:     []string{"id", "number"},
+				baseRepoFn: nil,
+			},
+			httpStub: func(r *httpmock.Registry) {
+				r.Register(
+					httpmock.GraphQL(`query PullRequestByNumber\b`),
+					httpmock.StringResponse(`{"data":{"repository":{
+						"pullRequest":{"number":13}
+					}}}`))
+			},
+			wantPR:   13,
+			wantRepo: "https://example.org/OWNER/REPO",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reg := &httpmock.Registry{}
+			defer reg.Verify(t)
+			if tt.httpStub != nil {
+				tt.httpStub(reg)
+			}
+
+			f := finder{
+				httpClient: func() (*http.Client, error) {
+					return &http.Client{Transport: reg}, nil
+				},
+				baseRepoFn: tt.args.baseRepoFn,
+				branchFn:   tt.args.branchFn,
+				remotesFn:  tt.args.remotesFn,
+			}
+
+			pr, repo, err := f.Find(FindOptions{
+				Selector: tt.args.selector,
+				Fields:   tt.args.fields,
+			})
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Find() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if pr.Number != tt.wantPR {
+				t.Errorf("want pr #%d, got #%d", tt.wantPR, pr.Number)
+			}
+			repoURL := ghrepo.GenerateRepoURL(repo, "")
+			if repoURL != tt.wantRepo {
+				t.Errorf("want repo %s, got %s", tt.wantRepo, repoURL)
+			}
+		})
+	}
+}
